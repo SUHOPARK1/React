38c6b5ca8fc7e384ea6947a77f1a77523f6d6ae9
SUHOPARK1 <qkrtnghsla@naver.com>
2021-01-20 18:02:04 +0900

    2021.01.20

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-
 import './App.css'
+
+/* import Hello from './hello/Hello'
 import Calculator from './calc/Calculator'
-import NumberProvider from './calc/NumberProvider'
+import NumberProvider from './calc/NumberProvider'  */
+import ToDoInput from './todo/ToDoInput'
 
-const App = () => (<NumberProvider><Calculator/></NumberProvider> )
+const App = () => (<ToDoInput/>)
 
-export default App;
+/* const App = () => (<Hello/>) */
+
+export default App
--- src/todo/ToDoList.jsx
+++ src/todo/ToDoList.jsx
@@ -1,34 +1,8 @@
 import React, {useState} from 'react'
-import { useDispatch } from 'react-redux' 
 
 const ToDoList = () => {
-    const [todo, setTodo] = useState('')
-    const dispatch = useDispatch()
-    const addTodo = todo => dispatch(addTodoAction(todo))
-
-    const submitForm = () => {}
-    const handleChange = () => {}
-
     return <>
-        <form onSubmit = {submitForm} method="POST">
-            <div className="row mt-3">
-                <div className="form-group col-sm-8">
-                    <input type="text"g
-                           placeholder="할일 입력"
-                           name="todo"
-                           className="form-control"
-                           value={todo}
-                           onChange={handleChange}
-                    />
-                </div>
-                <div className="col-sm-4">
-                    <button type="submit" className="btn btn-primary btn-md">
-                        할일 추가
-                    </button>
-                </div>
-            </div>
-        </form>
+    
     </>
 }
-
 export default ToDoList
