fa5971861d04851318389b646e026d9f5156e51b
SUHOPARK1 <qkrtnghsla@naver.com>
2021-01-20 17:11:34 +0900

    2021.01.20

--- src/calc/Calculator.jsx
+++ src/calc/Calculator.jsx
@@ -22,19 +22,19 @@ const Calculator = () => (<CalculatorStyles>
         <NumberButton buttonValue={7} />
         <NumberButton buttonValue={8} />
         <NumberButton buttonValue={9} />
-        <FunctionButton buttonValue='*' />
+        <FunctionButton buttonValue={'*'} />
         <NumberButton buttonValue={4} />
         <NumberButton buttonValue={5} />
         <NumberButton buttonValue={6} />
-        <FunctionButton buttonValue='-' />
+        <FunctionButton buttonValue={'-'} />
         <NumberButton buttonValue={1} />
         <NumberButton buttonValue={2} />
         <NumberButton buttonValue={3} />
-        <FunctionButton buttonValue='+' />
+        <FunctionButton buttonValue={'+'} />
         <div className="zero-button">
         <NumberButton buttonValue={0} />
         </div>
-        <NumberButton buttonValue="."/>
+        <NumberButton buttonValue={"."}/>
         <EqualButton />
     </div>
 </CalculatorStyles>)
